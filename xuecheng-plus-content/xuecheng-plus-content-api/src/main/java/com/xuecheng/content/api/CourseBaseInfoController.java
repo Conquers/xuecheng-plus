cedc11fd39fb00c679bd8bd11cfc6537bc26e866
wtt <conquer1ss@gmail.com>
2023-04-30 14:31:39 +0800

    完成课程查询接口

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
@@ -4,28 +4,28 @@ import com.xuecheng.base.model.PageParams;
 import com.xuecheng.base.model.PageResult;
 import com.xuecheng.content.model.dto.QueryCourseParamsDto;
 import com.xuecheng.content.model.po.CourseBase;
+import com.xuecheng.content.service.CourseBaseInfoService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-
-@Api(value = "课程信息编辑接口",tags = "课程信息编辑接口")
+@Api(value = "课程信息编辑接口", tags = "课程信息编辑接口")
 @RestController
 public class CourseBaseInfoController {
 
+    @Autowired
+    CourseBaseInfoService courseBaseInfoService;
+
     @ApiOperation("课程查询接口")
-    @RequestMapping("/course/list")
-    public PageResult<CourseBase> list(PageParams pageParams, @RequestBody(required = false) QueryCourseParamsDto queryCourseParams) {
-        // 说明：pageParams分页参数通过url的key/value传入(地址栏)，queryCourseParams通过json数据传入(body)，使用@RequestBody注解将json转成QueryCourseParamsDto对象。
-        List<CourseBase> courseBases = new ArrayList<>();
-        CourseBase courseBase = new CourseBase();
-        courseBase.setName("Java");
-        courseBases.add(courseBase);
-        return new PageResult<>(courseBases,2,1,2);
+    @PostMapping("/course/list")
+    public PageResult<CourseBase> list(PageParams pageParams, @RequestBody QueryCourseParamsDto queryCourseParams) {
+
+        PageResult<CourseBase> pageResult = courseBaseInfoService.queryCourseBaseList(pageParams, queryCourseParams);
+
+        return pageResult;
 
     }
 
