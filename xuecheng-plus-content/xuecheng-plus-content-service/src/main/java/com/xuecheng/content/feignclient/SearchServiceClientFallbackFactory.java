db633c34009c70a432ea093a2409098147057a91
wtt <conquer1ss@gmail.com>
2023-05-24 20:44:50 +0800

    完成课程搜索和课程信息索引同步

---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/feignclient/SearchServiceClientFallbackFactory.java
@@ -0,0 +1,26 @@
+package com.xuecheng.content.feignclient;
+
+import com.xuecheng.content.model.dto.CourseIndex;
+import feign.hystrix.FallbackFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class SearchServiceClientFallbackFactory implements FallbackFactory<SearchServiceClient> {
+    @Override
+    public SearchServiceClient create(Throwable throwable) {
+
+        return new SearchServiceClient() {
+
+            @Override
+            public Boolean add(CourseIndex courseIndex) {
+                throwable.printStackTrace();
+                log.debug("调用搜索发生熔断走降级方法,熔断异常:", throwable.getMessage());
+
+                return false;
+            }
+        };
+    }
+}
+
--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/jobhandler/CoursePublishTask.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/jobhandler/CoursePublishTask.java
@@ -1,6 +1,11 @@
 package com.xuecheng.content.service.jobhandler;
 
 
+import com.xuecheng.base.execption.XueChengPlusException;
+import com.xuecheng.content.feignclient.SearchServiceClient;
+import com.xuecheng.content.mapper.CoursePublishMapper;
+import com.xuecheng.content.model.dto.CourseIndex;
+import com.xuecheng.content.model.po.CoursePublish;
 import com.xuecheng.content.service.CoursePublishService;
 import com.xuecheng.messagesdk.model.po.MqMessage;
 import com.xuecheng.messagesdk.service.MessageProcessAbstract;
@@ -8,6 +13,7 @@ import com.xuecheng.messagesdk.service.MqMessageService;
 import com.xxl.job.core.context.XxlJobHelper;
 import com.xxl.job.core.handler.annotation.XxlJob;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -26,6 +32,12 @@ public class CoursePublishTask extends MessageProcessAbstract {
 
     @Autowired
     CoursePublishService coursePublishService;
+
+    @Autowired
+    CoursePublishMapper coursePublishMapper;
+
+    @Autowired
+    SearchServiceClient searchServiceClient;
 
 
     //任务调度入口
@@ -94,12 +106,40 @@ public class CoursePublishTask extends MessageProcessAbstract {
 
     //保存课程索引信息
     public void saveCourseIndex(MqMessage mqMessage, long courseId) {
+
         log.debug("保存课程索引信息,课程id:{}", courseId);
-        try {
-            TimeUnit.SECONDS.sleep(2);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+
+        //消息id
+        Long id = mqMessage.getId();
+        //消息处理的service
+        MqMessageService mqMessageService = this.getMqMessageService();
+        //消息幂等性处理
+        int stageTwo = mqMessageService.getStageTwo(id);
+        if (stageTwo > 0) {
+            log.debug("课程索引已处理直接返回，课程id:{}", courseId);
+            return;
         }
+
+        Boolean result = saveCourseIndex(courseId);
+        if (result) {
+            //保存第一阶段状态
+            mqMessageService.completedStageTwo(id);
+        }
+    }
+
+    private Boolean saveCourseIndex(Long courseId) {
+
+        //取出课程发布信息
+        CoursePublish coursePublish = coursePublishMapper.selectById(courseId);
+        //拷贝至课程索引对象
+        CourseIndex courseIndex = new CourseIndex();
+        BeanUtils.copyProperties(coursePublish, courseIndex);
+        //远程调用搜索服务api添加课程信息到索引
+        Boolean add = searchServiceClient.add(courseIndex);
+        if (!add) {
+            XueChengPlusException.cast("添加索引失败");
+        }
+        return add;
 
     }
 
